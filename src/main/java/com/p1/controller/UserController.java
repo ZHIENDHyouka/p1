11d252ab4602591530efcd32c77f4b292d734179
panlll <760447402@qq.com>
2023-04-22 11:31:20 +0800

    整合

--- src/main/java/com/p1/controller/UserController.java
+++ src/main/java/com/p1/controller/UserController.java
@@ -2,6 +2,7 @@ package com.p1.controller;
 
 import com.p1.entity.ResultVO;
 import com.p1.entity.User;
+import com.p1.service.MovieService;
 import com.p1.service.UserSerivce;
 import com.p1.utils.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     @Autowired
     private UserSerivce userSerivce;
+    @Autowired
+    private MovieService movieService;
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     //http://localhost:8083/user/add
@@ -21,6 +24,13 @@ public class UserController {
         ResultVO resultVO = userSerivce.saveUser(user);
         return resultVO;
     }
+    //http://localhost:8083/user/movieList
+    @RequestMapping("/user/movieList")
+    public ResultVO list(){
+        ResultVO resultVO =movieService.getMovie();
+        return  resultVO;
+    }
+
 
 
 }
